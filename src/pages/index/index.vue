a9c80f74344139e79bb87fb83cceb3dd7ae9a457
StarHeartHunt <starheart233@gmail.com>
2023-03-15 14:14:35 +0800

    feat: page bottom navbar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,30 @@
 <template>
   <view class="index">
-    <Counter />
+    <view class="btm-nav">
+      <view class="btn nav-item">
+        <view class="icon-home h-5 w-5"></view>
+        <text class="btm-nav-label">主页</text>
+      </view>
+      <view class="btn nav-item active">
+        <view class="icon-warn h-5 w-5"></view>
+        <text class="btm-nav-label">信息</text>
+      </view>
+      <view class="btn nav-item">
+        <view class="icon-stats h-5 w-5"></view>
+        <text class="btm-nav-label">统计</text>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-import './index.scss'
-import Counter from '../../components/Counter.vue'
+import "./index.scss";
+import Counter from "../../components/Counter.vue";
 
 export default {
-  name: 'Index',
+  name: "Index",
   components: {
-    Counter
-  }
-}
+    Counter,
+  },
+};
 </script>
