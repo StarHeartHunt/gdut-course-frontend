28a61aebb8b0f1399ba9ccbe426126f0adb5a3a7
StarHeartHunt <starheart233@gmail.com>
2023-03-16 20:56:06 +0800

    refactor: use pure tailwind to write components

--- windi.config.js
+++ windi.config.js
@@ -4,7 +4,26 @@ export default {
     // 忽略部分文件夹
     exclude: ["node_modules", ".git", "dist"],
   },
-  plugins: [require("daisyui")],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#48B0F1",
+          50: "#7ae2ff",
+          100: "#70d8ff",
+          200: "#66ceff",
+          300: "#5cc4ff",
+          400: "#52bafb",
+          500: "#48b0f1",
+          600: "#3ea6e7",
+          700: "#349cdd",
+          800: "#2a92d3",
+          900: "#2088c9",
+        },
+      },
+    },
+  },
+  plugins: [],
   corePlugins: {
     // 禁用掉在小程序环境中不可能用到的 plugins
     container: false,
