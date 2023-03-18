3ae38f7401fdab84d5e86a8b32a42e81fbedfa75
StarHeartHunt <starheart233@gmail.com>
2023-03-18 19:23:35 +0800

    feat: add consts for weekdays

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+export const timetable = [
+  { number: "1", period: "08:30 09:15" },
+  { number: "2", period: "09:20 10:05" },
+  { number: "3", period: "10:25 11:10" },
+  { number: "4", period: "11:15 12:00" },
+  { number: "5", period: "13:50 14:35" },
+  { number: "6", period: "14:40 15:25" },
+  { number: "7", period: "15:30 16:15" },
+  { number: "8", period: "16:30 17:15" },
+  { number: "9", period: "17:20 18:05" },
+  { number: "10", period: "18:30 19:05" },
+  { number: "11", period: "19:20 20:05" },
+  { number: "12", period: "20:10 20:55" },
+]
+
+export const weekdays = [
+  "周二",
+  "周三",
+  "周四",
+  "周五",
+  "周六",
+  "周日",
+]
