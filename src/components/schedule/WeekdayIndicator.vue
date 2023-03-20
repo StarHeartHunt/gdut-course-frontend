28b0d5d39fbeb15da1204d244b4dd76246e16a69
StarHeartHunt <starheart233@gmail.com>
2023-03-20 19:28:03 +0800

    feat: calendar/weeks view & style tweaks

--- src/components/schedule/WeekdayIndicator.vue
+++ src/components/schedule/WeekdayIndicator.vue
@@ -1,35 +1,38 @@
 <template>
-  <view
-    class="w-full grid grid-cols-8 min-h-10 justify-space-between items-center content-center place-items-center bg-primary-500 shadow"
+  <scroll-view
+    class="flex flex-row bg-primary-500 shadow items-center justify-around border-box whitespace-nowrap "
+    :scroll-x="true"
+    style="width: 100%"
   >
-    <view class="fa-solid fa-arrow-right w-full text-center align-middle" style="color: white"></view>
-    <view class="text-center text-white"> 第{{ currentWeek }}周 </view>
     <view
-      class="text-center text-white"
-      v-for="(weekday, index) in weekdays"
+      class="text-center text-white m-3 inline-block h-auto"
+      v-for="(week, index) in allWeeks"
       :key="index"
-      >{{ weekday }}</view
+      >第{{ week }}周</view
     >
-  </view>
+  </scroll-view>
   <view
     class="w-full grid grid-cols-8 min-h-10 justify-space-between items-center content-center place-items-center"
   >
-    <view class="text-xs"> {{ currentMonth.toString() }}月 </view>
-    <view
-      class="text-xs text-center rounded-full p-1 w-4 h-4"
-      :style="{
-        color: weekDates[index] === currentDate ? 'white' : 'black',
-        backgroundColor: weekDates[index] === currentDate ? '#3B82F6' : 'unser',
-      }"
-      v-for="(weekDate, index) in weekDates"
-      :key="index"
-      >{{ weekDate.toString() }}</view
-    >
+    <view class="text-xs"> {{ (currentMonth + 1).toString() }}月 </view>
+    <view v-for="(weekDate, index) in weekDates" :key="index">
+      <view class="text-xs text-center mt-1">{{ weekdays[index] }}</view>
+      <view
+        class="text-xs text-center rounded-full p-1 w-4 h-4 mb-1"
+        :style="{
+          color: weekDates[index] === currentDate ? 'white' : 'black',
+          backgroundColor:
+            weekDates[index] === currentDate ? '#3B82F6' : 'unser',
+        }"
+      >
+        {{ weekDate.toString() }}
+      </view>
+    </view>
   </view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { computed, defineComponent } from "vue"
 import { weekdays } from "@/consts"
 import { arrayRange } from "@/utils/array"
 
@@ -42,16 +45,30 @@ dayjs.locale("zh-cn")
 
 export default defineComponent({
   props: {
-    currentWeek: Number,
+    currentWeek: {
+      type: Number,
+      required: true,
+    },
+    totalWeeksCount: {
+      type: Number,
+      required: true,
+    },
   },
-  setup() {
-    const currentMonth = dayjs().month() + 1
+  setup(props) {
+    const currentMonth = dayjs().month()
     const currentDate = dayjs().get("date")
     const weekStartDate = dayjs().weekday(0).get("date")
     const weekEndDate = dayjs().weekday(6).get("date")
     const weekDates = arrayRange(weekStartDate, weekEndDate, 1)
+    const allWeeks = computed(() => arrayRange(1, props.totalWeeksCount, 1))
 
-    return { currentMonth, currentDate, weekdays, weekDates }
+    return {
+      currentMonth,
+      currentDate,
+      weekdays,
+      weekDates,
+      allWeeks,
+    }
   },
 })
 </script>
--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -4,19 +4,22 @@
       class="w-full flex items-center content-center justify-center place-items-center"
     >
       <view
-        class="mx-2 my-2 flex flex-col box-border items-center w-11/12 drop-shadow-lg bg-white rounded clickable"
+        class="mx-2 my-2 flex flex-col box-border items-center w-full drop-shadow-lg bg-white rounded clickable"
         @tap="onTapUserLogin"
       >
         <view
-          class="flex middle none w-full center rounded-lg py-3 font-sans font-bold uppercase text-primary-900"
+          class="relative flex middle none w-full center rounded-lg h-9 py-1 font-sans font-bold uppercase text-primary-900"
         >
-          <image
-            class="mx-4"
-            src="/assets/images/a.png"
-            mode="widthFix"
-            style="width: 25px"
-          ></image>
-          <text class="justify-self-center self-center">用户管理</text>
+          <view
+            class="fa-solid fa-user w-9 mx-1 text-center align-middle justify-self-center self-center"
+          ></view>
+          <text
+            class="ml-1 text-center align-middle justify-self-center self-center"
+            >用户管理</text
+          >
+          <view
+            class="absolute fa-chevron-right fas text-primary-900 right-4 h-fit text-center align-middle justify-self-center self-center"
+          ></view>
         </view>
       </view>
     </view>
