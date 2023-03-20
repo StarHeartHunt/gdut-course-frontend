807238e561db454d99102f5defa00dc9d7f830b1
StarHeartHunt <starheart233@gmail.com>
2023-03-20 08:56:32 +0800

    build: enable `cssModule`

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from "path"
 
 const config = {
   projectName: "gdut-course",
@@ -21,7 +21,7 @@ const config = {
   ],
   defineConstants: {},
   alias: {
-    "@": path.resolve(__dirname, "..", "src")
+    "@": path.resolve(__dirname, "..", "src"),
   },
   copy: {
     patterns: [],
@@ -62,7 +62,7 @@ const config = {
         },
       },
       cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: "module", // 转换模式，取值为 global/module
           generateScopedName: "[name]__[local]___[hash:base64:5]",
